f1769dfa20e6f9608be3aab5ff29155112450a81
ralsei <fr2500@pm.me>
2024-11-29 18:35:00 +0300

    Implemented sumOfTwo

--- src/main/kotlin/sumoftwo.kt
+++ src/main/kotlin/sumoftwo.kt
@@ -2,5 +2,15 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    val map = HashMap<Int, Int>()
+    for (i in numbers.indices) {
+        val x = numbers[i]
+        val y = target - x
+        if (map.contains(y)) {
+            return intArrayOf(map.getValue(y), i)
+        }
+        map[x] = i
+    }
+
+    throw IllegalArgumentException()
 }
